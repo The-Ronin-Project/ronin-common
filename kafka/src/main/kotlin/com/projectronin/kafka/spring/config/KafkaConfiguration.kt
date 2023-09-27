11efe9e145a6e8bddfe130c92e27971397f76078
Carl Graving <carl@projectronin.com>
2023-09-27 13:12:19 -0400

    Fix Dead Letter producer issues

--- kafka/src/main/kotlin/com/projectronin/kafka/spring/config/KafkaConfiguration.kt
+++ kafka/src/main/kotlin/com/projectronin/kafka/spring/config/KafkaConfiguration.kt
@@ -1,14 +1,18 @@
 package com.projectronin.kafka.spring.config
 
 import com.projectronin.kafka.config.ClusterProperties
+import com.projectronin.kafka.handlers.DeadLetterProducer
+import io.micrometer.core.instrument.MeterRegistry
 import org.apache.kafka.common.security.auth.SecurityProtocol
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationContextAware
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Lazy
 
 @Configuration
-open class KafkaConfiguration {
+open class KafkaConfiguration : ApplicationContextAware {
 
     @Bean
     @Lazy
@@ -31,5 +35,9 @@ open class KafkaConfiguration {
             saslUsername = saslUsername,
             saslPassword = saslPassword
         )
+    }
+
+    override fun setApplicationContext(applicationContext: ApplicationContext) {
+        DeadLetterProducer.meterRegistry = applicationContext.getBean(MeterRegistry::class.java)
     }
 }
--- kafka/src/main/kotlin/com/projectronin/kafka/spring/config/ProducerConfiguration.kt
+++ kafka/src/main/kotlin/com/projectronin/kafka/spring/config/ProducerConfiguration.kt
@@ -3,19 +3,16 @@ package com.projectronin.kafka.spring.config
 import com.projectronin.kafka.clients.MeteredProducer
 import com.projectronin.kafka.config.ClusterProperties
 import com.projectronin.kafka.config.ProducerProperties
-import com.projectronin.kafka.handlers.DeadLetterProducer
 import io.micrometer.core.instrument.MeterRegistry
 import org.apache.kafka.clients.producer.KafkaProducer
 import org.apache.kafka.clients.producer.Producer
-import org.springframework.context.ApplicationContext
-import org.springframework.context.ApplicationContextAware
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Lazy
 import java.util.Optional
 
 @Configuration
-open class ProducerConfiguration : ApplicationContextAware {
+open class ProducerConfiguration {
     @Bean(name = ["defaultProducerProperties"])
     @Lazy
     open fun defaultProducerProperties(clusterProperties: ClusterProperties): ProducerProperties {
@@ -29,9 +26,5 @@ open class ProducerConfiguration : ApplicationContextAware {
         meterRegistry: Optional<MeterRegistry>
     ): Producer<String, T> {
         return MeteredProducer(KafkaProducer(producerProperties), meterRegistry.orElse(null))
-    }
-
-    override fun setApplicationContext(applicationContext: ApplicationContext) {
-        DeadLetterProducer.meterRegistry = applicationContext.getBean(MeterRegistry::class.java)
     }
 }
