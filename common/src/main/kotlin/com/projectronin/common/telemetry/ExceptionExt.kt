216286108d29f5179e96b56c62c29506efd00ebc
Carl Graving <carl@projectronin.com>
2023-10-07 22:12:07 -0400

    Add another extension for Throwable

--- common/src/main/kotlin/com/projectronin/common/telemetry/ExceptionExt.kt
+++ common/src/main/kotlin/com/projectronin/common/telemetry/ExceptionExt.kt
@@ -4,23 +4,13 @@ import io.opentracing.log.Fields
 import io.opentracing.tag.Tags
 import io.opentracing.util.GlobalTracer
 
-fun Exception.addToDDTraceSpan() {
-    GlobalTracer.get().activeSpan()?.apply {
-        setTag(Tags.ERROR, true)
-        log(
-            mutableMapOf(
-                Fields.ERROR_OBJECT to this@addToDDTraceSpan
-            )
-        )
-    }
-}
-
 fun Throwable.addToDDTraceSpan() {
     GlobalTracer.get().activeSpan()?.apply {
         setTag(Tags.ERROR, true)
         log(
             mutableMapOf(
-                Fields.MESSAGE to message
+                Fields.MESSAGE to message,
+                Fields.ERROR_OBJECT to this@addToDDTraceSpan
             )
         )
     }
