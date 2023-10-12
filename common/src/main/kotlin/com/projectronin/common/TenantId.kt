513fed895e7d78c3bd06adb15d8348fcb09a44e9
Brent Beardsley <brent@projectronin.com>
2023-10-12 13:42:44 -0600

    add tenant id validation and random fun

--- common/src/main/kotlin/com/projectronin/common/TenantId.kt
+++ common/src/main/kotlin/com/projectronin/common/TenantId.kt
@@ -1,4 +1,32 @@
 package com.projectronin.common
 
+import java.security.SecureRandom
+
 @JvmInline
-value class TenantId(val value: String)
+value class TenantId(val value: String) {
+    override fun toString() = value
+
+    init {
+        require(value in nonstandardIds || base32Regex.matches(value)) { "invalid tenant id: $value" }
+    }
+
+    companion object {
+        /**
+         * https://projectronin.atlassian.net/wiki/spaces/ENG/pages/1737556005/Organization+Ids
+         * Base32: https://www.crockford.com/base32.html
+         */
+        fun random() = TenantId(
+            buildString {
+                repeat(8) {
+                    val index = random.nextInt(base32Alphabet.length)
+                    append(base32Alphabet[index])
+                }
+            }
+        )
+    }
+}
+
+private val nonstandardIds = listOf( "apposnd", "mdaoc", "ronin")
+private const val base32Alphabet = "0123456789abcdefghjkmnpqrstvwxyz"
+private val base32Regex = Regex("^[${base32Alphabet}]{8}$")
+private val random = SecureRandom()
