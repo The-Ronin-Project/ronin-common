55030c268d0a00f6f4b05e5fdec42869633886f2
Brooks Boyd <brooks@projectronin.com>
2023-11-01 13:17:54 -0500

    Add tags for authentication context keys

--- common/src/main/kotlin/com/projectronin/common/telemetry/Tags.kt
+++ common/src/main/kotlin/com/projectronin/common/telemetry/Tags.kt
@@ -12,4 +12,10 @@ object Tags {
     const val RONIN_EVENT_SOURCE_TAG = "ronin.event.source"
     const val RONIN_EVENT_TYPE_TAG = "ronin.event.type"
     const val RONIN_EVENT_VERSION_TAG = "ronin.event.version"
+
+    const val AUTH_IS_AUTHENTICATED = "auth.is_authenticated"
+    const val AUTH_USER_TYPE = "auth.user_type"
+    const val AUTH_USER_ID = "auth.user_id"
+    const val AUTH_PATIENT_ID = "auth.session.patient_id"
+    const val AUTH_PROVIDER_ID = "auth.session.provider_id"
 }
