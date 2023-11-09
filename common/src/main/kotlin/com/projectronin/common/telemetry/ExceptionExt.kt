24c7fdab8e0f339a72e83ee9302c3617b77485f1
Ross M. Lodge <ross@projectronin.com>
2023-11-08 17:11:52 -0800

    DASH-6212: improve error handling by providing unique ids

--- common/src/main/kotlin/com/projectronin/common/telemetry/ExceptionExt.kt
+++ common/src/main/kotlin/com/projectronin/common/telemetry/ExceptionExt.kt
@@ -4,14 +4,18 @@ import io.opentracing.log.Fields
 import io.opentracing.tag.Tags
 import io.opentracing.util.GlobalTracer
 
-fun Throwable.addToDDTraceSpan() {
+fun Throwable.addToDDTraceSpan(exceptionUniqueId: String? = null) {
     GlobalTracer.get().activeSpan()?.apply {
         setTag(Tags.ERROR, true)
         log(
             mutableMapOf(
                 Fields.MESSAGE to message,
                 Fields.ERROR_OBJECT to this@addToDDTraceSpan
-            )
+            ).also {
+                if (exceptionUniqueId != null) {
+                    it[RoninFields.ERROR_ID] = exceptionUniqueId
+                }
+            }
         )
     }
 }
