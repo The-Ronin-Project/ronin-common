bd8f936ef4472daa8b8d861e865e871da2b8a7c1
Kent Grueneich <kent@projectronin.com>
2024-01-30 15:37:16 -0600

    Add annotation and exception handler

---
+++ kafka/src/main/kotlin/com/projectronin/kafka/handlers/LogAndRestartExceptOnMissingTopicExceptionHandler.kt
@@ -0,0 +1,22 @@
+package com.projectronin.kafka.handlers
+
+import com.projectronin.common.telemetry.addToDDTraceSpan
+import mu.KLogger
+import mu.KotlinLogging
+import org.apache.kafka.streams.errors.MissingSourceTopicException
+import org.apache.kafka.streams.errors.StreamsUncaughtExceptionHandler
+
+class LogAndRestartExceptOnMissingTopicExceptionHandler : StreamsUncaughtExceptionHandler {
+    private val logger: KLogger = KotlinLogging.logger { }
+
+    override fun handle(exception: Throwable?): StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse {
+        if (exception is MissingSourceTopicException) {
+            logger.error("A Kafka Topic did not exist. $exception")
+            exception.addToDDTraceSpan()
+            return StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.SHUTDOWN_CLIENT
+        }
+        logger.error("Some Exception that was not properly handled in the stream. \n$exception")
+        exception?.addToDDTraceSpan()
+        return StreamsUncaughtExceptionHandler.StreamThreadExceptionResponse.REPLACE_THREAD
+    }
+}
