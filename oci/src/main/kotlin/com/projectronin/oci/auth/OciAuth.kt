7bab103a41e8899cdb65784915dfc3be41eb7b89
Carl Graving <carl@projectronin.com>
2024-03-01 11:21:23 -0500

    DASH-7441 Add region to Auth provider to remove reion warnings

--- oci/src/main/kotlin/com/projectronin/oci/auth/OciAuth.kt
+++ oci/src/main/kotlin/com/projectronin/oci/auth/OciAuth.kt
@@ -1,5 +1,6 @@
 package com.projectronin.oci.auth
 
+import com.oracle.bmc.Region
 import com.oracle.bmc.auth.SimpleAuthenticationDetailsProvider
 import com.projectronin.oci.OciProperties
 import java.util.Base64
@@ -11,5 +12,6 @@ class OciAuth(private val ociProperties: OciProperties) {
         .userId(ociProperties.user)
         .fingerprint(ociProperties.fingerprint)
         .privateKeySupplier { Base64.getDecoder().decode(ociProperties.privateKey).inputStream() }
+        .region(Region.fromRegionCode(ociProperties.primaryRegion))
         .build()
 }
